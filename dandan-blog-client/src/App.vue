99b56019e58f1255f02d64663f39aa2a7fb52e46
Ben <892156860@qq.com>
2017-07-15 23:32:15 +0800

    提交代码

---
+++ dandan-blog-client/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  mounted(){
+    this.$ajax.get('http://192.168.1.105:7001/api/v1/user', {
+      // csrfToken: '22'
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+      // alert("11");
+    });
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
